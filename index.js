efe9125e9035573c65de0df3e518fcb2cc93b7b7
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-23 14:33:14 -0800

    user.js

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
+// step 1 npm init --yes 
+// step 2 npm install apollo-server graphql mongoose nodemon
+// step 3 npm install mongodb
+
+
+
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs'); 
 const resolvers = require('./graphql/resolvers');
 
-const MONGODB =
-  "mongodb+srv://haroutyunhaltunyan93:F6EYxlEDC6A2vJbW@clusterreactloginprojec.z1zgk.mongodb.net/?retryWrites=true&w=majority&appName=Clusterreactloginproject3";
+const MONGODB = 
+"mongodb+srv://haroutyunhaltunyan93:RMEizsGcjSnfT7VF@cluster1.cw144.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+
+
+// note to self resolve this issue
+
+
 // Don't mess with this, connects to MongoDB with my auth credentials
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://haroutyunhaltunyan93:RMEizsGcjSnfT7VF@cluster1.cw144.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
 
 const server = new ApolloServer({
   typeDefs,
