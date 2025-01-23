564d4aaf71bad18cb5710f5ebebee05332ec1a5b
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-23 12:05:20 -0800

    index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const { ApolloServer } = require('apollo-server');
+ const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const typeDefs = require('./grapghql/typeDefs');
+const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
-const MONGODB = "mongodb+srv://haroutyunhaltunyan93:F6EYxlEDC6A2vJbW@clusterreactloginprojec.z1zgk.mongodb.net/?retryWrites=true&w=majority&appName=Clusterreactloginproject3"
-// dont mess with this^, connects to mongodb with my auth credentials 
+// MongoDB connection string with your credentials
+const MONGODB = "mongodb+srv://haroutyunhaltunyan93:F6EYxlEDC6A2vJbW@clusterreactloginprojec.z1zgk.mongodb.net/?retryWrites=true&w=majority&appName=Clusterreactloginproject3";
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true }) // Added `useUnifiedTopology` for better compatibility
     .then(() => {
-        console.log ("MongoDB COnnected");
-        return server.listen({port: 5000});
+        console.log("MongoDB Connected");
+        return server.listen({ port: 5000 });
     })
     .then((res) => {
-        console.log(`Server running at ${res.url}`)
+        console.log(`Server running at ${res.url}`);
+    })
+    .catch((err) => {
+        console.error("Connection error", err);
     });
