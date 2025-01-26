aeb0e5270320a4347b63fae0251fe0ecccf8038f
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-25 21:09:02 -0800

    json, app.js, apolloclient.js, authcontext.js, index.js, authcontext.js, apolloclient.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,104 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import client from './context/apolloClient'; // Adjusted path for Apollo Client
+import { ApolloProvider } from '@apollo/client'; // Correct import for ApolloProvider
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './authContext'; // Ensure the path is correct
 
+// React 18: Use ReactDOM.createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ApolloProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// //import client from '/apolloClient';
+
+
+// import client from './context/apolloClient'
+
+
+
+// // import client from './utils/apolloClient'; 
+// // import { ApolloProvider } from '@apollo/client';
+
+
+
+// import { ApolloProvider } from '@apollo/react-hooks';
+// import { BrowserRouter } from 'react-router-dom';
+// import { AuthProvider } from './authContext';
+
+
+// //Our React application needs access to...
+// // Client
+// // Authorization Context                     //
+// // Browser Router (React ROuter) /logIn /register
+
+// ReactDOM.render(
+//   <AuthProvider>
+//   <ApolloProvider client={client}>
+//     <BrowserRouter>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </BrowserRouter>
+// </ApolloProvider>
+// </AuthProvider>,
+// document.getElementById('root')
+// );
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+
+
+
+
+
---
+++ client/src/pages/register.js
@@ -0,0 +1,21 @@
+import {useContext, useState} from "react"
+import { authContext } from "../authContext";
+import { useForm } from "../utility/hooks";
+import { useMutation } from "@apollo/react-hooks";
+
+import {gql} from 'graphql-tag';
+import { useNavigate } from "react-router-dom";
+
+const REGISTER_USER = gql`
+    mutation Mutation(
+        $registerInput: RegisterInput
+    ) {
+        registerUser(
+            registerInput: $registerInput
+        ) {
+            email
+            username
+            token
+        }
+    }
+`
