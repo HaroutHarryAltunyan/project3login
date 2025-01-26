8147435a0897eff63ffcac7a89c66255721a65a9
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-25 21:45:48 -0800

    json, app.js, navbar,..... homepage and navbar is live

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Homepage from './pages/homepage'
+import Homepage from './pages/homepage';
+import Navbar from './components/navbar';
 
-function Home() {
-  return <div>Welcome to the Home Page!</div>;
-}
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage/>} />
+        <Route path="/login" element={<Homepage/>} />
+        <Route path="/register" element={<Homepage/>} />
       </Routes>
     </div>
   );
