eaba9dd39d90d8393ac653fc9f0aa9c3f13f620b
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-27 18:14:49 -0800

    react-vite folders

---
+++ logintailwind/backend/graphql/resolvers/index.js
@@ -0,0 +1,14 @@
+const messagesResolvers = require('./messages');
+const usersResolvers = require('./users');
+
+
+module.exports = {
+    Query: {
+        ...messagesResolvers.Query, 
+        ...usersResolvers.Query
+    },
+    Mutation: {
+        ...messagesResolvers.Mutation,
+        ...usersResolvers.Mutation
+    },
+};
